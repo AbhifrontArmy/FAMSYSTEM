1e5791c499af4dd024153f868a1baa804cd1ed57
Abhi Patel <76242769+AbhiPatel10@users.noreply.github.com>
2022-03-01 15:19:39 +0530

    [ADD] Alert

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import React from 'react';
-import { Switch, BrowserRouter as Router, Route, Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
 
-
-import Login from './pages/login';
-import Role from './pages/role';
-import Dashboard from './pages/dashboard';
-import Project from './pages/project';
-import User from './pages/user';
-import Add from './pages/addrole';
-import Adduser from './pages/adduser';
-import Profile from './pages/profile';
-import Layout from './components/layout/index';
-import Pagination from './pages/pagination';
-import Updaterole from './pages/updaterole';
+import Login from "./pages/login";
+import Role from "./pages/role";
+import Dashboard from "./pages/dashboard";
+import Project from "./pages/project";
+import User from "./pages/user";
+import AddRole from "./pages/addrole";
+import Adduser from "./pages/adduser";
+import Profile from "./pages/profile";
+import Layout from "./components/layout/index";
+import Alert from "./components/Alert";
 
 function App() {
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 2500);
+  };
   return (
     <>
       <Router>
+        <Alert alert={alert} />
         <Switch>
           <Route path="/" exact component={Login} />
           <Layout>
@@ -25,12 +34,11 @@ function App() {
             <Route path="/role" exact component={Role} />
             <Route path="/project" exact component={Project} />
             <Route path="/user" exact component={User} />
-            <Route path="/addrole" exact component={Add} />
+            <Route path="/addrole" exact>
+              <AddRole showAlert={showAlert}/>
+            </Route>
             <Route path="/adduser" exact component={Adduser} />
             <Route path="/profile" exact component={Profile} />
-            <Route path="/Pagination" exact component={Pagination} />
-            <Route path="/Updaterole" exact component={Updaterole} />
-
           </Layout>
         </Switch>
       </Router>
--- src/pages/addrole/index.jsx
+++ src/pages/addrole/index.jsx
@@ -1,46 +1,64 @@
 import React, { useEffect, useState } from "react";
 import { Form } from "semantic-ui-react";
-import './addrole.css';
+import "./addrole.css";
 import { Link } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import axios from 'axios';
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
 
-const Add = () => {
-    const [name, setName] = useState("");
+const AddRole = (props) => {
+  const [name, setName] = useState("");
 
-    const handleSubmit = () => {
-        console.log({ name })
+  const handleSubmit = () => {
+    console.log({ name });
 
-        axios.post(`https://famsystem.herokuapp.com/api/role`, {
-            name,
-        })
+    axios
+      .post(`https://famsystem.herokuapp.com/api/role`, {
+        name,
+      })
+      .then((getData) => {
+        console.log("Post role data", getData.data.message);
+        props.showAlert(`${getData.data.message}`, "success");
+    })
+    .catch(({ response }) => { 
+        console.log(response.data);  
+        props.showAlert(`${response.data.message}`, "danger");
+    })
+      ;
+  };
 
-    };
+  return (
+    <div className="add">
+      <Form>
+        <Form.Field>
+          <div>
+            {" "}
+            <strong>Role-Profile</strong>
+          </div>
+          <br />
+          <b>Name :</b>
+          <input
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Enter your Name"
+          />
+        </Form.Field>
+        <button
+          type="button"
+          class="btn btn-success m-4"
+          onClick={handleSubmit}
+        >
+          Create
+        </button>
+        {/* <button type="button" class="btn btn-success m-4">Update</button> */}
+        <Link to="/user">
+          <button type="button" class="btn btn-danger">
+            Back
+          </button>
+        </Link>
+      </Form>
+    </div>
+  );
+};
 
-
-    return (
-        <div className="add">
-            <Form>
-                <Form.Field>
-                    <div> <strong>Role-Profile</strong></div>
-                    <br />
-                    <b>Name :</b>
-                    <input
-                        name="name"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        placeholder="Enter your Name"
-                    />
-                </Form.Field>
-                <button type="button" class="btn btn-success m-4" onClick={handleSubmit}>Create</button>
-                {/* <button type="button" class="btn btn-success m-4">Update</button> */}
-                <Link to="/user">
-                    <button type="button" class="btn btn-danger">Back</button>
-                </Link>
-            </Form>
-        </div>
-    );
-}
-
-
-export default Add;
+export default AddRole;
